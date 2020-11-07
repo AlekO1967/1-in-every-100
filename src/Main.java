b8785bd06921f9615b486fd75c8ba06782d763dc
Aleksandr Osipov <eltisprim@gmail.com>
2020-11-07 12:02:58 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Добавьте сумму в рублях, без мелочи, мелочь ми нэ виговариваэм:");
+        int transfer = input.nextInt();
+        int bonus;
+        int limit = 1000;
+                if (transfer >= limit) {
+            bonus = transfer / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        int startBalanse = 100;
+        int count = startBalanse + transfer + bonus;
+        System.out.println("Бонус: " + bonus);
+        System.out.println("На Вашем счету: " + count );
+    }
 }
